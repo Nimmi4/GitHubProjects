110201e7021ddfbd71c60cb73acde611eab14d2e
Nimmi4 <56770148+Nimmi4@users.noreply.github.com>
2019-10-19 20:48:50 -0700

    commit
    commit

---
+++ Projectpractice/src/main/java/GitHub/Project/App.java
@@ -0,0 +1,13 @@
+package GitHub.Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
